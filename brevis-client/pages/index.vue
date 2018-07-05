7f96d77450379794deb77ed4ca66175e3bf19c7a
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-05 13:49:02 -0700

    Minor tag removal updates.

--- brevis-client/pages/index.vue
+++ brevis-client/pages/index.vue
@@ -1,19 +1,15 @@
 <template>
   <section class="container-condensed">
     <URLInput/>
-    <br/>
-    <URLResult/>
   </section>
 </template>
 
 <script>
 import URLInput from '~/components/URLInput.vue'
-import URLResult from '~/components/URLResult.vue'
 
 export default {
   components: {
-    URLInput,
-    URLResult
+    URLInput
   }
 }
 </script>
