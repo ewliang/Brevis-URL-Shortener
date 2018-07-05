94bf69f5528b35a069fdeb7fd30d299ddaf48c85
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-04 23:04:18 -0700

    Added method for handling posting user input of URL to be shorten via axios backend post call.

--- brevis-client/components/URLInput.vue
+++ brevis-client/components/URLInput.vue
@@ -1,6 +1,6 @@
 <template>
-  <form id = "urlInputForm">
-    <input type = "url" class = "form-control" id = "urlInputFormControl" placeholder = "e.g.: www.eric-liang.com"/>
+  <form id = "urlInputForm" v-on:submit = "generateShortURL">
+    <input type = "url" v-model = "inputURL" class = "form-control" id = "urlInputFormControl" placeholder = "e.g.: www.eric-liang.com"/>
     <input type = "submit" class = "btn btn-primary" value = "Shrink!"/>
   </form><!--end -->
 </template>
@@ -9,7 +9,28 @@
 import axios from 'axios'
 
 export default {
-
+  data () {
+    return {
+      inputURL: ''
+    }
+  },
+  methods: {
+    generateShortURL () {
+      var longURL = {
+        originalURL: this.inputURL
+      }
+      alert(this.inputURL)
+      console.log(this.inputURL)
+      axios.post(`http://localhost:4000/api/`, longURL)
+      .then(response => {
+        console.log("Successfully generated ShortURL!!!")
+        console.log("SHORT URL: " + response)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  }
 }
 </script>
 
