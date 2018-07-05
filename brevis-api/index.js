95dabce21cd4f87d89005f9fee4a5b7451e0ebeb
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-04 23:03:24 -0700

    Refactored vars -> const. Added cors and mongoose connection configuration code.

--- brevis-api/index.js
+++ brevis-api/index.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-var apiRoutes = require('./routes/api');
+const mongoose = require('mongoose');
+const databaseConfig = require('./config/config');
 
-var app = express();
+const apiRoutes = require('./routes/api');
+
+const app = express();
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(databaseConfig.database.connectionURL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'There is an error while attempting to connect to the database.'));
+db.once('open', function() {
+  console.log('Successfully connected to the database ' + db.name + '.');
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
