96d3b2200988fb643ae17e47adbad43df373ea32
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-06 09:44:55 -0700

    Initial commit. Added code to call backend to retrieve original URL on page load via asyncData, then mounted to call a method to handle redirection.

---
+++ brevis-client/pages/_shortenURL.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    SHORTED REQUEST
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData ({ params }) {
+    var url = await axios.get(`http://localhost:4000/api/${params.shortenURL}`)
+    return {
+      originalURL: url.data
+    }
+  },
+  methods: {
+    goRedirect () {
+      try {
+        // Redirect to original external URL
+        window.location = this.originalURL
+      }
+      catch(e) {
+        console.log("ERROR: " + e)
+      }
+    }
+  },
+  mounted () {
+    this.goRedirect()
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
